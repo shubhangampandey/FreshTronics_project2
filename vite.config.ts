372893a52144e2b52ef298c5690db61cc215b06d
Shubhangam <shubhangampandey1@gmail.com>
2025-11-30 15:50:25 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "url";
+
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: true, 
+    
+  },
   publicDir: "./static",
   base: "./",
   css: {
@@ -13,7 +18,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
